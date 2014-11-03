604eb0df0edd540fc5ce052c44689326d7d6cde4
Evan Huus <eapache@gmail.com>
2014-11-03 20:06:54 +0000

    Change default permissions to 0660

--- lib/semian.rb
+++ lib/semian.rb
@@ -66,7 +66,7 @@ class Semian
     # +timeout+: Default timeout in seconds.
     #
     # Returns the registered resource.
-    def register(name, tickets: 0, permissions: 0600, timeout: 1)
+    def register(name, tickets: 0, permissions: 0660, timeout: 1)
       resource = Resource.new(name, tickets, permissions, timeout)
       resources[name] = resource
     end
