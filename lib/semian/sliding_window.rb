5242878c38f909b344cfbdf34d39c2e78c495944
Kye Wei <kye.wei@shopify.com>
2015-10-01 15:02:46 -0400

    Refactor naming, some shared dependencies

--- lib/semian/sliding_window.rb
+++ lib/semian/sliding_window.rb
@@ -1,19 +1,28 @@
 module Semian
   class SlidingWindow #:nodoc:
-    def initialize(name, max_window_length, permissions)
+    def initialize(name, max_window_size, permissions)
       if respond_to?(:_initialize)
-        _initialize(name, max_window_length, permissions)
+        _initialize(name, max_window_size, permissions)
       else
         @successes = 0
-        @max_window_length = max_window_length
+        @max_window_size = max_window_size
         @window = []
       end
     end
 
     # For anyone consulting this, the array stores an integer amount of seconds since epoch
     # Use Time.at(_time_ms_ / 1000) to convert to time
+
+    # A sliding window is a structure that keeps at most @max_window_size recent timestamps
+    # in store, like this: if @max_window_size = 4, current time is 10, @window =[5,7,9,10].
+    # Another push of (11) at 11 sec would make @window [7,9,10,11], popping off 5.
+
     def self.shared?
       false
+    end
+
+    def max_window_size
+      @max_window_size
     end
 
     def successes
@@ -25,11 +34,11 @@ module Semian
     end
 
     def semid
-      0
+      -1
     end
 
     def shmid
-      0
+      -1
     end
 
     def size
@@ -41,7 +50,7 @@ module Semian
     end
 
     def push(time_ms)
-      @window.shift while @window.size >= @max_window_length
+      @window.shift while @window.size >= @max_window_size
       @window << time_ms
     end
 
@@ -54,7 +63,7 @@ module Semian
     end
 
     def unshift (time_ms)
-      @window.pop while @window.size >= @max_window_length
+      @window.pop while @window.size >= @max_window_size
       @window.unshift(time_ms)
     end
 
@@ -69,6 +78,5 @@ module Semian
     def last
       @window.last
     end
-
   end
 end
