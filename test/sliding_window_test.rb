92ed019be890d543b0f7845531345c6b61ddb3b8
Kye Wei <kye.wei@shopify.com>
2015-11-09 17:25:38 +0000

    Changed test style

--- test/sliding_window_test.rb
+++ test/sliding_window_test.rb
@@ -1,18 +1,29 @@
 require 'test_helper'
 
 class TestSlidingWindow < MiniTest::Unit::TestCase
-  def test_sliding_window_push
-    run_test_with_sliding_window_classes do
+  CLASS = ::Semian::SlidingWindow
+
+  def setup
+    @sliding_window = CLASS.new(6,
+                                name: 'TestSlidingWindow',
+                                permissions: 0660)
+    @sliding_window.clear
+  end
+
+  def teardown
+    @sliding_window.destroy
+  end
+
+  module SlidingWindowTestCases
+    def test_sliding_window_push
       assert_equal(0, @sliding_window.size)
       @sliding_window << 1
       assert_correct_first_and_last_and_size(@sliding_window, 1, 1, 1, 6)
       @sliding_window << 5
       assert_correct_first_and_last_and_size(@sliding_window, 1, 5, 2, 6)
     end
-  end
 
-  def test_sliding_window_resize
-    run_test_with_sliding_window_classes do
+    def test_sliding_window_resize
       assert_equal(0, @sliding_window.size)
       @sliding_window << 1 << 2 << 3 << 4 << 5 << 6
       assert_correct_first_and_last_and_size(@sliding_window, 1, 6, 6, 6)
@@ -23,10 +34,8 @@ class TestSlidingWindow < MiniTest::Unit::TestCase
       @sliding_window.resize_to 6
       assert_correct_first_and_last_and_size(@sliding_window, 2, 6, 5, 6)
     end
-  end
 
-  def test_sliding_window_edge_falloff
-    run_test_with_sliding_window_classes do
+    def test_sliding_window_edge_falloff
       assert_equal(0, @sliding_window.size)
       @sliding_window << 0 << 1 << 2 << 3 << 4 << 5 << 6 << 7
       assert_correct_first_and_last_and_size(@sliding_window, 2, 7, 6, 6)
@@ -35,38 +44,26 @@ class TestSlidingWindow < MiniTest::Unit::TestCase
     end
   end
 
-  private
+  module SlidingWindowUtilityMethods
+    def assert_correct_first_and_last_and_size(sliding_window, first, last, size, max_size)
+      assert_equal(first, sliding_window.first)
+      assert_equal(last, sliding_window.last)
+      assert_equal(size, sliding_window.size)
+      assert_equal(max_size, sliding_window.max_size)
+    end
 
-  def sliding_window_classes
-    @classes ||= [::Semian::SlidingWindow]
-  end
-
-  def run_test_with_sliding_window_classes(klasses = sliding_window_classes)
-    klasses.each do |klass|
-      begin
-        @sliding_window = klass.new(6,
-                                    name: 'TestSlidingWindow',
-                                    permissions: 0660)
-        @sliding_window.clear
-        yield(klass)
-      ensure
-        @sliding_window.destroy
-      end
+    def assert_sliding_windows_in_sync(sliding_window_1, sliding_window_2)
+      # it only exposes ends, size, and max_size, so can only check those
+      assert_equal(sliding_window_1.first, sliding_window_2.first)
+      assert_equal(sliding_window_1.last, sliding_window_2.last)
+      assert_equal(sliding_window_1.size, sliding_window_2.size)
+      assert_equal(sliding_window_1.max_size, sliding_window_2.max_size)
     end
   end
 
-  def assert_correct_first_and_last_and_size(sliding_window, first, last, size, max_size)
-    assert_equal(first, sliding_window.first)
-    assert_equal(last, sliding_window.last)
-    assert_equal(size, sliding_window.size)
-    assert_equal(max_size, sliding_window.max_size)
-  end
+  include SlidingWindowTestCases
 
-  def assert_sliding_windows_in_sync(sliding_window_1, sliding_window_2)
-    # it only exposes ends, size, and max_size, so can only check those
-    assert_equal(sliding_window_1.first, sliding_window_2.first)
-    assert_equal(sliding_window_1.last, sliding_window_2.last)
-    assert_equal(sliding_window_1.size, sliding_window_2.size)
-    assert_equal(sliding_window_1.max_size, sliding_window_2.max_size)
-  end
+  private
+
+  include SlidingWindowUtilityMethods
 end
