30c31e238596ee45fe2e06dc8aafc723850058fe
Dale Hamel <dale.hamel@shopify.com>
2017-02-12 18:30:29 -0500

    Refactor semian.c includes and types into header files
    - Refactor semian includes into semian.h
    - Refactor semian types into semian_types.h

---
+++ ext/semian/semian.h
@@ -0,0 +1,30 @@
+/*
+System, 3rd party, and project includes
+
+Implements Init_semian, which is used as C/Ruby entrypoint.
+*/
+
+#ifndef SEMIAN_H
+#define SEMIAN_H
+
+// System includes
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+
+// 3rd party includes
+#include <openssl/sha.h>
+#include <ruby.h>
+#include <ruby/util.h>
+#include <ruby/io.h>
+
+//semian includes
+#include <semian_types.h>
+
+void Init_semian();
+
+#endif //SEMIAN_H
