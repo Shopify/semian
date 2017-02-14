86c743ddb88a7460b5c595dabfeac38ab5a5c269
Dale Hamel <dalehamel@users.noreply.github.com>
2017-02-14 09:27:47 -0500

    Don't require sudo for travis (#110)
    * Don't require sudo for travis
    
    * Be more consistent between toxiproxi bin and deb

--- scripts/install_toxiproxy.sh
+++ scripts/install_toxiproxy.sh
@@ -5,9 +5,14 @@ if which toxiproxy > /dev/null; then
   exit 0
 fi
 
-if which apt-get > /dev/null; then
+if [ -n "${CI}" ]; then
+  wget -O /tmp/toxiproxy https://github.com/Shopify/toxiproxy/releases/download/v2.1.0/toxiproxy-server-linux-amd64
+  chmod +x /tmp/toxiproxy
+  nohup /tmp/toxiproxy &
+  exit 0
+elif which apt-get > /dev/null; then
   echo "Installing toxiproxy"
-  wget -O /tmp/toxiproxy.deb https://github.com/Shopify/toxiproxy/releases/download/v2.0.0/toxiproxy_2.0.0_amd64.deb
+  wget -O /tmp/toxiproxy.deb https://github.com/Shopify/toxiproxy/releases/download/v2.1.0/toxiproxy_2.1.0_amd64.deb
   sudo dpkg -i /tmp/toxiproxy.deb
   sudo service toxiproxy start
   exit 0
