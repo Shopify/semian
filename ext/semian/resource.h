b93d0097498309f46ee82a816ce32b55ec076736
Dale Hamel <dale.hamel@shopify.com>
2017-02-14 11:16:02 -0500

    Move WITHOUT_GVL definition to semian.h

--- ext/semian/resource.h
+++ ext/semian/resource.h
@@ -4,20 +4,9 @@ For core semian resource functions exposed directly to ruby.
 Functions here are associated with rubyland operations.
 */
 
-
-// Defines for ruby threading primitives
-#if defined(HAVE_RB_THREAD_CALL_WITHOUT_GVL) && defined(HAVE_RUBY_THREAD_H)
-// 2.0
-#include <ruby/thread.h>
-#define WITHOUT_GVL(fn,a,ubf,b) rb_thread_call_without_gvl((fn),(a),(ubf),(b))
-#elif defined(HAVE_RB_THREAD_BLOCKING_REGION)
- // 1.9
-typedef VALUE (*my_blocking_fn_t)(void*);
-#define WITHOUT_GVL(fn,a,ubf,b) rb_thread_blocking_region((my_blocking_fn_t)(fn),(a),(ubf),(b))
-#endif
-
 #ifndef SEMIAN_RESOURCE_H
 #define SEMIAN_RESOURCE_H
+
 #include "semian.h" // FIXME remove this once temporary declarations are removed
 #include "types.h"
 
--- ext/semian/semian.h
+++ ext/semian/semian.h
@@ -22,6 +22,18 @@ Implements Init_semian, which is used as C/Ruby entrypoint.
 #include "types.h"
 #include "resource.h"
 
+// FIXME: This is needed here temporarily
+// Defines for ruby threading primitives
+#if defined(HAVE_RB_THREAD_CALL_WITHOUT_GVL) && defined(HAVE_RUBY_THREAD_H)
+// 2.0
+#include <ruby/thread.h>
+#define WITHOUT_GVL(fn,a,ubf,b) rb_thread_call_without_gvl((fn),(a),(ubf),(b))
+#elif defined(HAVE_RB_THREAD_BLOCKING_REGION)
+ // 1.9
+typedef VALUE (*my_blocking_fn_t)(void*);
+#define WITHOUT_GVL(fn,a,ubf,b) rb_thread_blocking_region((my_blocking_fn_t)(fn),(a),(ubf),(b))
+#endif
+
 VALUE eSyscall, eTimeout, eInternal;
 
 void Init_semian();
