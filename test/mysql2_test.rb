5644523afcb9dbbe4a364e9c9fefe6ad2eaeacbc
Angelo Albiero Neto <angeloalbiero@gmail.com>
2017-06-16 10:53:55 -0400

    Make dependencies hosts configurable

--- test/mysql2_test.rb
+++ test/mysql2_test.rb
@@ -25,7 +25,7 @@ class TestMysql2 < Minitest::Test
   end
 
   def test_semian_can_be_disabled
-    resource = Mysql2::Client.new(semian: false).semian_resource
+    resource = Mysql2::Client.new(host: TOXIPROXY_HOST, port: MYSQL_TOXIC_PORT, semian: false).semian_resource
     assert_instance_of Semian::UnprotectedResource, resource
   end
 
@@ -251,7 +251,7 @@ class TestMysql2 < Minitest::Test
   end
 
   def test_unconfigured
-    client = Mysql2::Client.new(host: '127.0.0.1', port: '13306')
+    client = Mysql2::Client.new(host: TOXIPROXY_HOST, port: MYSQL_TOXIC_PORT)
     assert_equal 2, client.query('SELECT 1 + 1 as sum;').to_a.first['sum']
   end
 
@@ -281,8 +281,8 @@ class TestMysql2 < Minitest::Test
     Mysql2::Client.new(
       connect_timeout: 1,
       read_timeout: 1,
-      host: '127.0.0.1',
-      port: '13306',
+      host: TOXIPROXY_HOST,
+      port: MYSQL_TOXIC_PORT,
       semian: SEMIAN_OPTIONS.merge(semian_options),
     )
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,23 +11,43 @@ require 'helpers/background_helper'
 require 'helpers/circuit_breaker_helper'
 require 'helpers/resource_helper'
 require 'helpers/adapter_helper'
+require 'byebug'
 
 Semian.logger = Logger.new(nil)
+
+TOXIPROXY_HOST = 'toxiproxy'
+TOXIPROXY_PORT = 8474
+TOXIPROXY_URL = "http://#{TOXIPROXY_HOST}:#{TOXIPROXY_PORT}"
+
+MYSQL_HOST = 'mysql'
+MYSQL_PORT = 3306
+MYSQL_TOXIC_PORT = 13306
+
+REDIS_HOST = 'redis'
+REDIS_PORT = 6379
+REDIS_TOXIC_PORT = 16379
+
+NET_HTTP_HOST = 'library'
+NET_HTTP_PORT = 31050
+NET_HTTP_TOXIC_PORT = 31051
+
+Toxiproxy.host = TOXIPROXY_URL
+
 Toxiproxy.populate([
   {
     name: 'semian_test_mysql',
-    upstream: 'localhost:3306',
-    listen: 'localhost:13306',
+    upstream: "#{MYSQL_HOST}:#{MYSQL_PORT}",
+    listen: "#{TOXIPROXY_HOST}:#{MYSQL_TOXIC_PORT}",
   },
   {
     name: 'semian_test_redis',
-    upstream: 'localhost:6379',
-    listen: 'localhost:16379',
+    upstream: "#{REDIS_HOST}:#{REDIS_PORT}",
+    listen: "#{TOXIPROXY_HOST}:#{REDIS_TOXIC_PORT}",
   },
   {
     name: 'semian_test_net_http',
-    upstream: 'localhost:31050',
-    listen: 'localhost:31051',
+    upstream: "#{NET_HTTP_HOST}:#{NET_HTTP_PORT}",
+    listen: "#{TOXIPROXY_HOST}:#{NET_HTTP_TOXIC_PORT}",
   },
 ])
 
