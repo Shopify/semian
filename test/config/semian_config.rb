68597549b99d0a6224f438945ec46a61592b81a9
Angelo Albiero Neto <angeloalbiero@gmail.com>
2017-06-21 15:40:32 -0400

    Simplify configuration

--- test/config/semian_config.rb
+++ test/config/semian_config.rb
@@ -9,21 +9,7 @@ class SemianConfig
     end
 
     def all
-      @entries ||= begin
-        entries = YAML.load_file(CONFIG_FILE)
-        flatten_keys(entries)
-      end
-    end
-
-    private
-
-    def flatten_keys(entries, object = {}, parent_key = '')
-      entries.each_with_object(object) do |(key, value), hash|
-        next flatten_keys(value, hash, key) if value.is_a?(Hash)
-
-        hash["#{parent_key}_#{key}"] = value
-        hash
-      end
+      @entries ||= YAML.load_file(CONFIG_FILE)
     end
   end
 end
