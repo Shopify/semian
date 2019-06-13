569a551e14e04a81149d0b1af97bd697800173e0
Michael Kipper <michael.kipper@shopify.com>
2019-06-13 16:41:08 -0400

    Added a dprintf function

--- ext/semian/util.h
+++ ext/semian/util.h
@@ -1,8 +1,24 @@
 #ifndef EXT_SEMIAN_UTIL_H
 #define EXT_SEMIAN_UTIL_H
 
+#include <stdarg.h>
+#include <stdio.h>
+
 #include <openssl/sha.h>
 #include <ruby.h>
+
+#ifdef DEBUG
+#  define DEBUG_TEST 1
+#else
+#  define DEBUG_TEST 0
+#endif
+
+#define dprintf(fmt, ...) \
+  do { \
+    if (DEBUG_TEST) { \
+      printf("[DEBUG] %s:%d" fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__); \
+    } \
+  } while (0)
 
 const char* check_id_arg(VALUE id);
 
