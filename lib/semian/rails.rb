470b6355ad7c811aa052456ffc77656b0112576e
Nikita Vasilevsky <nvasilevskii@gmail.com>
2022-03-01 10:07:10 -0500

    Support new @raw_connection ivar name (#309)

--- lib/semian/rails.rb
+++ lib/semian/rails.rb
@@ -2,6 +2,8 @@ require 'active_record/connection_adapters/abstract_adapter'
 
 class ActiveRecord::ConnectionAdapters::AbstractAdapter
   def semian_resource
-    @connection.semian_resource
+    # support for https://github.com/rails/rails/commit/d86fd6415c0dfce6fadb77e74696cf728e5eb76b
+    connection = instance_variable_defined?(:@raw_connection) ? @raw_connection : @connection
+    connection.semian_resource
   end
 end
