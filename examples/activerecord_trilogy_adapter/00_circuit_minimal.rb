7487a687403a39b01ba57fbe16554a2c8fb56d1b
Michael Nikitochkin <miry.sof@gmail.com>
2023-02-09 18:26:38 +0100

    Create a sample example how to configure Trilogy adapter
    Introduce rake task to run examples for activerecrod_trilogy_adapter.

---
+++ examples/activerecord_trilogy_adapter/00_circuit_minimal.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "activerecord-trilogy-adapter"
+require "semian"
+require "semian/activerecord_trilogy_adapter"
+require_relative "../colors"
+
+puts "> Starting example #{__FILE__}".blue.bold
+
+SEMIAN_PARAMETERS = {
+  circuit_breaker: true,
+  success_threshold: 1,
+  error_threshold: 3,
+  error_timeout: 3,
+  bulkhead: false,
+}
+
+configuration = {
+  adapter: "trilogy",
+  username: "root",
+  host: ENV.fetch("MYSQL_HOST", "localhost"),
+  port: Integer(ENV.fetch("MYSQL_PORT", 3306)),
+  database: "mysql",
+  semian: SEMIAN_PARAMETERS,
+}
+
+adapter = ActiveRecord::ConnectionAdapters::TrilogyAdapter.new(configuration)
+adapter.execute("SELECT 1;")
+
+puts "> That's all Folks!".green
