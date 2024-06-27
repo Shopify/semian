f37cc01ae0454cc71a1da1cd3d674cad435a7660
Dan Mayer <dan.mayer@shopify.com>
2024-06-27 09:03:23 -0400

    update based on feedback

--- test/adapters/redis_client_test.rb
+++ test/adapters/redis_client_test.rb
@@ -135,6 +135,7 @@ module RedisClientTests
     assert_includes(Semian.resources.keys, :redis_shared_pool_one)
     assert_includes(Semian.resources.keys, :redis_shared_pool_two)
 
+  ensure
     # clean up semians between test runs as we aren't using the standard semian names
     Semian.destroy(:redis_shared_pool_one)
     Semian.destroy(:redis_shared_pool_two)
