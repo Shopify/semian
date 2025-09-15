d56a396f514504135ae5225006b546a4afb9c2b8
Nirmit Parikh <nirmit.parikh@shopify.com>
2025-09-15 13:22:58 -0400

    Add semian resource name to logs

--- lib/semian/configuration_validator.rb
+++ lib/semian/configuration_validator.rb
@@ -32,7 +32,7 @@ module Semian
       if @force_config_validation
         raise ArgumentError, message
       else
-        Semian.logger.warn("[SEMIAN_CONFIG_WARNING]: #{message}")
+        Semian.logger.warn("[SEMIAN_CONFIG_WARNING]: Semian Adapter #{@name}: #{message}")
       end
     end
 
