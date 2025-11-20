2e57df0a1941f10fca8900c6c3a7b62cc7e81000
Kristofer Gaudel <kris.gaudel@shopify.com>
2025-11-20 00:17:59 -0500

    Protective Ruby wrapper to prevent flow

--- lib/semian.rb
+++ lib/semian.rb
@@ -19,6 +19,7 @@ require "semian/simple_integer"
 require "semian/simple_state"
 require "semian/lru_hash"
 require "semian/configuration_validator"
+require "semian/shared_memory"
 
 #
 # === Overview
